eb733526009c23887e00e0a6bb929c3cac77d4a0
Ruoyu <53182543+ruoyuwang1995ucas@users.noreply.github.com>
2022-08-20 00:43:50 +0800

    Update main_mpi.f90

--- main_mpi.f90
+++ main_mpi.f90
@@ -7,7 +7,7 @@ implicit none
 include 'mpif.h'
 
 integer:: proc_Rank, proc_Tot, ierror ! mpi parameters
-real (kind=8),allocatable :: w0_list(:),w_wt_arr(:,:),jdos_arr(:),jdos_sub(:)
+real (kind=8),allocatable :: w0_list(:),w_wt_arr(:,:),jdos_arr(:),jdos_arr_decay(:),jdos_sub(:)
 real(kind=8) :: sig,blk_1,blk_2,jdos,jdos_decay
 integer:: num_w0
 integer:: sub_size ! the size of sub w_list
@@ -24,12 +24,15 @@ num_w0=size(w0_list)
 !sub_size=3
 allocate(jdos_sub(sub_size))
 allocate(jdos_arr(num_w0))
+allocate(jdos_arr_decay())
 
-! set initial values of jdos_arr
+! set initial values of jdos_arr & jdos_arr_decay
 do i=1,num_w0
   jdos_arr(i)=w0_list(i)
+  jdos_arr_decay(i)=w0_list(i)
 enddo
 
+! calculate adsorption process
 ! initialize MPI 
 call MPI_INIT(ierror)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, proc_Tot,ierror)
@@ -59,6 +62,31 @@ endif
 
 call MPI_FINALIZE(ierror)
 
+! calculate the decay process
+call MPI_INIT(ierror)
+call MPI_COMM_SIZE(MPI_COMM_WORLD, proc_Tot,ierror)
+call MPI_COMM_RANK(MPI_COMM_WORLD, proc_Rank, ierror) 
+! scatter data to proc 0-3
+call MPI_Scatter(jdos_arr_decay, sub_size, MPI_DOUBLE_PRECISION, jdos_sub, sub_size, MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierror);
+! task per proc
+
+do j=1,sub_size
+  call jdos_w0(jdos_sub(j),blk_1,blk_2,w_wt_arr,jdos)
+  jdos_sub(j)=jdos
+  print *,jdos,'after', proc_Rank
+enddo
+
+call MPI_Gather(jdos_sub_decay,sub_size,MPI_DOUBLE_PRECISION, jdos_arr, sub_size, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD,ierror)
+
+if (proc_Rank == 0) then
+  open(2,file='jdos.dat',action='write', status='replace')
+  do k = 1,num_w0
+    write(2,*) w0_list(k), jdos_arr(k) 
+  enddo
+  close(2)
+endif
+
+call MPI_FINALIZE(ierror)
 end program pdos
 
 
